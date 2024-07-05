d53b99a14d8ab9f450e1dcb1839cbe2a895af330
sohumdalal <dsohum@gmail.com>
2024-07-05 17:19:21 -0400

    working on media queries

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -4,18 +4,21 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="carousel">
-      <div className="pre">Sohum Dalal.</div>
-      <div className="engineerPlus">engineer +</div>
-      <div className="change_outer">
-        <div className="change_inner">
-          <div className="element">craftsman</div>
-          <div className="element">chef</div>
-          <div className="element">baker</div>
-          <div className="element">barista</div>
-          <div className="element">farmer</div>
-          <div className="element">craftsman</div> {/* Repeat first element */}
+      <div className="myName">Sohum Dalal.</div>
+      <div className = "subTitle">
+        <div className="engineerPlus">engineer +</div>
+        <div className="change_outer">
+          <div className="change_inner">
+            <div className="element">craftsman</div>
+            <div className="element">chef</div>
+            <div className="element">baker</div>
+            <div className="element">barista</div>
+            <div className="element">farmer</div>
+            <div className="element">craftsman</div> {/* Repeat first element */}
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
