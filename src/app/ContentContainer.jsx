31587b110fef47d5a04ee37c0a9ce1562efa97c3
sohumdalal <dsohum@gmail.com>
2024-07-08 17:44:53 -0400

    done with media queries

--- src/app/ContentContainer.jsx
+++ src/app/ContentContainer.jsx
@@ -6,7 +6,7 @@ const ContentContainer = () => {
       <div className="bio">
         <p id="helloBlurb">Hello! Thanks for stopping by.</p>
         <p id="infoParagraph" className="mt-5">
-          I’m Sohum. I’m a software engineer and craftsman. I appreciate technology systems that are well-organized, functional first, and modular. Outside of a development environment, you can find me applying the same principles to other pursuits, namely baking bread. You can learn more about me and my work at the links below:
+          I’m Sohum. I’m a software engineer and craftsman. I appreciate technology systems that are well-organized, functional first, and modular. Outside of a development environment, you can find me applying the same principles to other pursuits, namely baking bread. You can learn more about me and my work at the links below, or you can email me <a href="mailto:dsohum@gmail.com" target="_blank" className="text-black no-underline relative hover:underline">here↗</a>
         </p>
         <ul className="list-none mt-10 p-0">
           <li className="mt-4 text-[0.9rem]"><a href="https://www.linkedin.com/in/sohum-dalal-985076a6/" target="_blank" className="text-black no-underline relative hover:underline"><span>LinkedIn</span>↗</a></li>
@@ -18,12 +18,11 @@ const ContentContainer = () => {
           <li className="mt-4 text-[0.9rem]"><a href="https://www.pinterest.com/dsohum/" target="_blank" className="text-black no-underline relative hover:underline"><span>Pinterest</span>↗</a></li>
         </ul>
       </div>
-      <div className="text-center mt-24 text-[0.8rem] text-gray-500 font-light">
+      <div className="text-top mt-20 text-[0.8rem] text-gray-500 font-light">
         <p>Created by Sohum Dalal with Next.js. 2024</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default ContentContainer
-
+export default ContentContainer;
