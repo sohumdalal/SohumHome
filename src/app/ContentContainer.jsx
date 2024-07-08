0c261027ecc94ad0d81e62cef7ae6b7e0b511612
sohumdalal <dsohum@gmail.com>
2024-07-08 07:10:40 -0400

    tailwind config

--- src/app/ContentContainer.jsx
+++ src/app/ContentContainer.jsx
@@ -1,24 +1,54 @@
+// import React from 'react'
+
+// const ContentContainer = () => {
+//   return (
+//     <div className="contentContainer">
+//       <div className="bio">
+//         <p id="helloBlurb">Hello! Thanks for stopping by.</p>
+//         <p id="infoParagraph">
+//           I’m Sohum. I’m a software engineer and craftsman. I appreciate technology systems that are well-organized, functional first, and modular to enable future innovation. Outside of a development environment, you can find me applying the same principles to other pursuits, namely baking bread. You can learn more about me and my work at the links below:
+//         </p>
+//         <ul>
+//           <li><a href="https://www.linkedin.com/in/sohum-dalal-985076a6/" target="_blank"><span>LinkedIn</span>↗</a></li>
+//           <li><a href="https://github.com/sohumdalal" target="_blank"><span>GitHub</span>↗</a></li>
+//           <li><a href="https://www.instagram.com/sohumdalal/" target="_blank"><span>Instagram</span>↗</a></li>
+//           <li><a href="https://twitter.com/SohumDalal" target="_blank"><span>X</span>↗</a></li>
+//           <li><a href="https://medium.com/@sohumdalal" target="_blank"><span>Medium</span>↗</a> (technical work)</li>
+//           <li><a href="https://substack.com/@sohumdalal" target="_blank"><span>Substack</span>↗</a> (other thoughts)</li>
+//           <li><a href="https://www.pinterest.com/dsohum/" target="_blank"><span>Pinterest</span>↗</a></li>
+//         </ul>
+//       </div>
+//       <div className="colophon">
+//         <p>Created by Sohum Dalal with Next.js. 2024</p>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ContentContainer
+
+
 import React from 'react'
 
 const ContentContainer = () => {
   return (
-    <div className="contentContainer">
+    <div className="flex flex-col h-[78%] w-[90%] mt-10 text-[calc(18px-2px)] font-light">
       <div className="bio">
-        <p id = "helloBlurb">Hello! Thanks for stopping by.</p>
-        <p id = "infoParagraph">
+        <p id="helloBlurb">Hello! Thanks for stopping by.</p>
+        <p id="infoParagraph" className="mt-5">
           I’m Sohum. I’m a software engineer and craftsman. I appreciate technology systems that are well-organized, functional first, and modular to enable future innovation. Outside of a development environment, you can find me applying the same principles to other pursuits, namely baking bread. You can learn more about me and my work at the links below:
         </p>
-        <ul>
-          <li><a href="https://www.linkedin.com/in/sohum-dalal-985076a6/" target="_blank"><span>LinkedIn</span>↗</a></li>
-          <li><a href="https://github.com/sohumdalal" target="_blank"><span>GitHub</span>↗</a></li>
-          <li><a href="https://www.instagram.com/sohumdalal/" target="_blank"><span>Instagram</span>↗</a></li>
-          <li><a href="https://twitter.com/SohumDalal" target="_blank"><span>X</span>↗</a></li>
-          <li><a href="https://medium.com/@sohumdalal" target="_blank"><span>Medium</span>↗</a> (technical work)</li>
-          <li><a href="https://substack.com/@sohumdalal" target="_blank"><span>Substack</span>↗</a> (other thoughts)</li>
-          <li><a href="https://www.pinterest.com/dsohum/" target="_blank"><span>Pinterest</span>↗</a></li>
+        <ul className="list-none mt-10 p-0">
+          <li className="mt-4 text-[0.9rem]"><a href="https://www.linkedin.com/in/sohum-dalal-985076a6/" target="_blank" className="text-black no-underline relative hover:underline"><span>LinkedIn</span>↗</a></li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://github.com/sohumdalal" target="_blank" className="text-black no-underline relative hover:underline"><span>GitHub</span>↗</a></li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://www.instagram.com/sohumdalal/" target="_blank" className="text-black no-underline relative hover:underline"><span>Instagram</span>↗</a></li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://twitter.com/SohumDalal" target="_blank" className="text-black no-underline relative hover:underline"><span>X</span>↗</a></li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://medium.com/@sohumdalal" target="_blank" className="text-black no-underline relative hover:underline"><span>Medium</span>↗</a> (technical work)</li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://substack.com/@sohumdalal" target="_blank" className="text-black no-underline relative hover:underline"><span>Substack</span>↗</a> (other thoughts)</li>
+          <li className="mt-4 text-[0.9rem]"><a href="https://www.pinterest.com/dsohum/" target="_blank" className="text-black no-underline relative hover:underline"><span>Pinterest</span>↗</a></li>
         </ul>
       </div>
-      <div className="colophon">
+      <div className="text-center mt-24 text-[0.8rem] text-gray-500 font-light">
         <p>Created by Sohum Dalal with Next.js. 2024</p>
       </div>
     </div>
--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,24 +1,51 @@
+// 'use client'
+// import React from 'react';
+
+// const Header = () => {
+//   return (
+//     <div className="carousel">
+//       <div className="myName">Sohum Dalal.</div>
+//       <div className = "subTitle">
+//         <div className="engineerPlus">engineer +</div>
+//         <div className="change_outer">
+//           <div className="change_inner">
+//             <div className="element">craftsman</div>
+//             <div className="element">chef</div>
+//             <div className="element">baker</div>
+//             <div className="element">barista</div>
+//             <div className="element">farmer</div>
+//             <div className="element">craftsman</div> {/* Repeat first element */}
+//           </div>
+//         </div>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+
 'use client'
 import React from 'react';
 
 const Header = () => {
   return (
-    <div className="carousel">
-      <div className="myName">Sohum Dalal.</div>
-      <div className = "subTitle">
-        <div className="engineerPlus">engineer +</div>
-        <div className="change_outer">
-          <div className="change_inner">
-            <div className="element">craftsman</div>
-            <div className="element">chef</div>
-            <div className="element">baker</div>
-            <div className="element">barista</div>
-            <div className="element">farmer</div>
-            <div className="element">craftsman</div> {/* Repeat first element */}
+    <div className="flex items-end h-[22%] w-[90%] font-light tracking-wider pb-2.5 bg-[#E1D8D5]">
+      <div className="text-[28px] mr-2.5 relative bottom-[calc((28px-18px)/2)]">Sohum Dalal.</div>
+      <div className="flex items-end">
+        <div className="text-[18px] mr-2.5">engineer +</div>
+        <div className="flex items-end text-[18px] overflow-hidden h-[18px]">
+          <div className="relative animate-rotate">
+            <div className="h-[18px] leading-[18px]">craftsman</div>
+            <div className="h-[18px] leading-[18px]">chef</div>
+            <div className="h-[18px] leading-[18px]">baker</div>
+            <div className="h-[18px] leading-[18px]">barista</div>
+            <div className="h-[18px] leading-[18px]">farmer</div>
+            <div className="h-[18px] leading-[18px]">craftsman</div> {/* Repeat first element */}
           </div>
         </div>
       </div>
-
     </div>
   );
 }
