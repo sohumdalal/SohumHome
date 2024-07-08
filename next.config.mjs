e65eec97d703f13becbccd39f945247a70c85264
sohumdalal <dsohum@gmail.com>
2024-07-08 06:57:48 -0400

    installed tailwind

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,19 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// module.exports = {
+//     future: {
+//       webpack5: true,
+//     },
+//     webpack: (config, { dev, isServer }) => {
+//       if (!isServer) {
+//         config.node = {
+//           fs: 'empty'
+//         }
+//       }
+//       return config
+//     }
+//   }
+  
